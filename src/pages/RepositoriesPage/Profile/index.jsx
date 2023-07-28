86a9c4af1f664884ca268e96b8408517432edcfa
IceRed0991 <wernergutschow@hotmail.com>
2023-07-28 16:49:30 -0300

    Creating Json Data

--- src/pages/RepositoriesPage/Profile/index.jsx
+++ src/pages/RepositoriesPage/Profile/index.jsx
@@ -1,29 +1,46 @@
 import React from "react";
+import  PropTypes  from "prop-types";
 
 import { MdGroup, MdLocationCity, MdWork, MdLink } from "react-icons/md";
 
 import{Container, Header, Avatar, Login, Name, Inner, Data} from './styles';
 
-export default function Profile () {
+export default function Profile ({user}) {
   return (
      <Container>
         <Header>
-        <Avatar src="https://avatars.githubusercontent.com/u/16406369?v=4"/>
-        <Login>IceRed0991</Login>
-        <Name>Werner Eduard Gutschow</Name>
+        <Avatar src={user.avatar_url}/>
+        <Login>{user.login}</Login>
+        <Name>{user.name}</Name>
         </Header>
         <Inner>
           <Data><MdGroup size={20}/>
-            30 <i>&nbsp; seguidores &nbsp; </i> &middot; 10 <i>&nbsp;seguindo</i>
+          {user.following} <i>&nbsp; seguidores &nbsp; </i> &middot; {user.followers} <i>&nbsp;Seguindo</i>
           </Data>
-          <Data><MdWork size={20}/> Unimed</Data>
-          <Data><MdLocationCity size={20}/> Assis - SP</Data>
-          <Data>
-            <MdLink size={20}/>
-            <a href="https://www.linkedin.com/in/werner-eduard-gutschow-72a513213/">Linkedin</a>
-          </Data>
+          {user.company &&
+          <Data><MdWork size={20}/> {user.company}</Data>
+        }
+        {user.location &&
+          <Data><MdLocationCity size={20}/> {user.location}</Data>
+        }
+        {user.blog &&
+          <Data><MdLink size={20}/><a href={`\\${user.blog}`}>{user.blog}</a></Data>
+        }
         </Inner>
 
       </Container>
   );
 }
+
+Profile.propTypes = {
+  user: PropTypes.shape({
+    login:PropTypes.string.isRequired,
+    name:PropTypes.string.isRequired,
+    avatar_url: PropTypes.string.isRequired,
+    followers:PropTypes.number.isRequired,
+    following: PropTypes.number.isRequired,
+    company: PropTypes.string,
+    blog: PropTypes.string,
+    location: PropTypes.string,
+  }).isRequired,
+};
--- src/pages/RepositoriesPage/index.jsx
+++ src/pages/RepositoriesPage/index.jsx
@@ -8,12 +8,22 @@ import Repositories from "./Repositories";
 
 
 export default function RepositoriesPage() {
+  const user={
+    login:"IceRed0991",
+    name:"Werner Eduard Gutschow",
+    avatar_url: "https://avatars.githubusercontent.com/u/16406369?v=4",
+    followers:1,
+    following: 1,
+    company: 'Unimed',
+    blog: "https://www.linkedin.com/in/werner-eduard-gutschow-72a513213/",
+    location: 'Assis -SP',
+  };
 
   return (
 
     <Container>
       <Sidebar>
-       <Profile/>
+       <Profile user ={user}/>
        <Filter/>
       </Sidebar>
       <Main>
