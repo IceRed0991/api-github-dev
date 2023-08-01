7176d8e4b5cbd06e3d140223c4a8dd6ca6ebc5f9
IceRed0991 <wernergutschow@hotmail.com>
2023-08-01 20:14:28 -0300

    Filter parte1

--- src/pages/RepositoriesPage/index.jsx
+++ src/pages/RepositoriesPage/index.jsx
@@ -19,12 +19,36 @@ export default function RepositoriesPage() {
     location: 'Assis -SP',
   };
 
+  // eslint-disable-next-line no-unused-vars
+  const repositories=[
+    {name:'Repo 1', description: 'Descrição', html_url:'https://www.linkedin.com/in/werner-eduard-gutschow-72a513213/',
+  language: 'JavaScript'},
+  {name:'Repo 2', description: 'Descrição', html_url:'https://www.linkedin.com/in/werner-eduard-gutschow-72a513213/',
+  language: 'JavaScript'},
+  {name:'Repo 3', description: 'Descrição', html_url:'https://www.linkedin.com/in/werner-eduard-gutschow-72a513213/',
+  language: 'PHP'},
+  {name:'Repo 4', description: 'Descrição', html_url:'https://www.linkedin.com/in/werner-eduard-gutschow-72a513213/',
+  language: 'Ruby'},
+  {name:'Repo 5', description: 'Descrição', html_url:'https://www.linkedin.com/in/werner-eduard-gutschow-72a513213/',
+  language: 'Java'},
+  {name:'Repo 5', description: 'Descrição', html_url:'https://www.linkedin.com/in/werner-eduard-gutschow-72a513213/',
+  language: 'Typescript'},
+  ];
+
+
+  const languages = [
+    { name: 'JavaScript', count: 2, color: '#f1c40f' },
+    { name: 'Shell', count: 2, color: '#95a5a6' },
+    { name: 'PHP', count: 2, color: '#3498db' },
+    { name: 'Ruby', count: 1, color: '#e74c3c' },
+  ];
+
   return (
 
     <Container>
       <Sidebar>
        <Profile user ={user}/>
-       <Filter/>
+       <Filter languages={languages}/>
       </Sidebar>
       <Main>
         <Repositories/>
